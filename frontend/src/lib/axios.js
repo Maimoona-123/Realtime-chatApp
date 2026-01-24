d0b567075c0cc44379041de1b09f0a76aecafdbb
Maimoona <maimonaaltaf7@gmail.com>
2026-01-24 22:48:47 +0500

    fix: add env variables

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
