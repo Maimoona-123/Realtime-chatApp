b4e6d4c5fa5a3066f11a573ce0852e60c565e3db
Maimoona <maimonaaltaf7@gmail.com>
2026-01-24 22:26:48 +0500

    fix: fix the env naming covention

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL,
   withCredentials: true,
 });
--- frontend/src/store/UseAuth.js
+++ frontend/src/store/UseAuth.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL
+const BASE_URL = process.env.REACT_APP_SERVER_URL
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
